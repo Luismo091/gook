da8ae5c4be5bb775d8b40e1614f53f4863464926
AndresD4rk <abarrera2@udi.edu.co>
2022-11-05 23:34:56 -0500

    123

--- proyectorden/GOOK/Pruebaimg.php
+++ proyectorden/GOOK/Pruebaimg.php
@@ -4,10 +4,10 @@ $query ="SELECT * FROM Usuario WHERE idUsu='1'";
 $resul = $conexion->query($query);
 $contlibro;
 if ($row = $resul->fetch_array()) {
- $contenido = $row['foto'];
+ $contenido = $row["foto"];
  $contlibro=base64_encode($contenido);
  ?>
-     <img src="data:image/png;base64,<?=base64_encode($row['foto'])?>" />
+     <img src="data:image/png;base64,<?=base64_encode($row["foto"])?>" />
       <?php
 }
 
