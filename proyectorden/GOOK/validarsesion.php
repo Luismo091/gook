50b77a9d67ab3469b5075e70ca0de2c7eb31afee
AndresD4rk <abarrera2@udi.edu.co>
2022-11-05 20:48:00 -0500

    123

--- proyectorden/GOOK/validarsesion.php
+++ proyectorden/GOOK/validarsesion.php
@@ -8,7 +8,12 @@ if (empty($_POST['inputEmail']) || empty($_POST['inputPassword'])) {
     $email = $_POST['inputEmail'];
     $clave = $_POST['inputPassword'];
 include 'conexion.php';
-$query ="SELECT * FROM Seguridad WHERE email='$email' AND clave='$clave'";
+$query ="SELECT idSeg, email, clave, 
+Rol_idRol,idUsu, nom1, nom2, ape1, ape2, 
+eda, foto, Seguridad_idSeg, Favorito_idFav,
+ Suscripcion_idSus, Reciente_idReciente 
+ FROM Seguridad INNER JOIN Usuario ON Seguridad.idSeg=Usuario.Seguridad_idSeg
+WHERE Seguridad.email='$email' AND  Seguridad.clave='$clave'";
 $resul = $conexion->query($query);
 $contlibro;
 if ($row = $resul->fetch_array()) {
