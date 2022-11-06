d0d7d1c3a0247c02344341e19dea07175f933500
AndresD4rk <abarrera2@udi.edu.co>
2022-11-05 23:31:33 -0500

    312

--- proyectorden/GOOK/main.php
+++ proyectorden/GOOK/main.php
@@ -62,7 +62,9 @@
           <a class="nav-link dropdown-toggle text-muted pr-0" href="#" id="navbarDropdownMenuLink" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="avatar avatar-sm mt-2">
-            <img src="data:image/jpg;base64,<?php echo base64_encode($_SESSION['foto']) ?>">
+
+          
+            <img src="data:image/jpg;base64,<?=base64_encode($_SESSION['foto']) ?>">
             </span>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
--- proyectorden/GOOK/perfil.php
+++ proyectorden/GOOK/perfil.php
@@ -364,7 +364,28 @@
               <div class="row mt-5 align-items-center">
                 <div class="col-md-3 text-center mb-5">
                   <div class="avatar avatar-xl">
-                    <img src="./assets/avatars/face-1.jpg" alt="..." class="avatar-img rounded-circle">
+
+
+
+
+                  <?php
+include 'conexion.php';
+$query ="SELECT * FROM Usuario WHERE idUsu='1'";
+$resul = $conexion->query($query);
+$contlibro;
+if ($row = $resul->fetch_array()) {
+ $contenido = $row['foto'];
+ $contlibro=base64_encode($contenido);
+echo $contlibro;
+ echo '<img src="data:image/png;base64,'.$contlibro.' width="100%" height="100%" />';//Parametros del libro
+}
+
+
+?>
+
+
+
+                   
                   </div>
                 </div>
                 <div class="col">
