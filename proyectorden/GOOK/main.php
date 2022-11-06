80051d743e9f33e96834bfc2bee65e6b610e364f
Luismo091 <101841546+Luismo091@users.noreply.github.com>
2022-11-05 22:24:30 -0500

    s

--- proyectorden/GOOK/main.php
+++ proyectorden/GOOK/main.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
 
@@ -12,9 +11,7 @@
   <!-- Simple bar CSS -->
   <link rel="stylesheet" href="css/simplebar.css">
   <!-- Fonts CSS -->
-  <link
-    href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <!-- Icons CSS -->
   <link rel="stylesheet" href="css/feather.css">
   <link rel="stylesheet" href="css/select2.css">
@@ -38,8 +35,7 @@
         <i class="fe fe-menu navbar-toggler-icon"></i>
       </button>
       <form class="form-inline mr-auto searchform text-muted">
-        <input class="form-control mr-sm-2 bg-transparent border-0 pl-4 text-muted" type="search"
-          placeholder="Busca algo..." aria-label="Search">
+        <input class="form-control mr-sm-2 bg-transparent border-0 pl-4 text-muted" type="search" placeholder="Busca algo..." aria-label="Search">
       </form>
       <ul class="nav">
         <li class="nav-item">
@@ -59,10 +55,9 @@
           </a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle text-muted pr-0" href="#" id="navbarDropdownMenuLink" role="button"
-            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-muted pr-0" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="avatar avatar-sm mt-2">
-              <img src="./assets/avatars/face-1.jpg" alt="..." class="avatar-img rounded-circle">
+              <img src="" alt="..." class="avatar-img rounded-circle">
             </span>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
@@ -80,8 +75,7 @@
         <!-- nav bar -->
         <div class="w-100 mb-4 d-flex">
           <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html">
-            <svg version="1.1" id="logo" class="navbar-brand-img brand-sm" xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120" xml:space="preserve">
+            <svg version="1.1" id="logo" class="navbar-brand-img brand-sm" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120" xml:space="preserve">
               <g>
                 <polygon class="st0" points="78,105 15,105 24,87 87,87 	" />
                 <polygon class="st0" points="96,69 33,69 42,51 105,51 	" />
@@ -101,12 +95,10 @@
                 <a class="nav-link pl-3" href="./index.html"><span class="ml-1 item-text">Default</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link pl-3" href="./dashboard-analytics.html"><span
-                    class="ml-1 item-text">Analytics</span></a>
+                <a class="nav-link pl-3" href="./dashboard-analytics.html"><span class="ml-1 item-text">Analytics</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link pl-3" href="./dashboard-sales.html"><span
-                    class="ml-1 item-text">E-commerce</span></a>
+                <a class="nav-link pl-3" href="./dashboard-sales.html"><span class="ml-1 item-text">E-commerce</span></a>
               </li>
               <li class="nav-item">
                 <a class="nav-link pl-3" href="./dashboard-saas.html"><span class="ml-1 item-text">Saas
@@ -142,8 +134,7 @@
                 <a class="nav-link pl-3" href="./ui-buttons.html"><span class="ml-1 item-text">Buttons</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link pl-3" href="./ui-notification.html"><span
-                    class="ml-1 item-text">Notifications</span></a>
+                <a class="nav-link pl-3" href="./ui-notification.html"><span class="ml-1 item-text">Notifications</span></a>
               </li>
               <li class="nav-item">
                 <a class="nav-link pl-3" href="./ui-modals.html"><span class="ml-1 item-text">Modals</span></a>
@@ -179,8 +170,7 @@
                     Elements</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link pl-3" href="./form_validation.html"><span
-                    class="ml-1 item-text">Validation</span></a>
+                <a class="nav-link pl-3" href="./form_validation.html"><span class="ml-1 item-text">Validation</span></a>
               </li>
               <li class="nav-item">
                 <a class="nav-link pl-3" href="./form_wizard.html"><span class="ml-1 item-text">Wizard</span></a>
@@ -225,8 +215,7 @@
                 <a class="nav-link pl-3" href="./chart-chartjs.html"><span class="ml-1 item-text">Chartjs</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link pl-3" href="./chart-apexcharts.html"><span
-                    class="ml-1 item-text">ApexCharts</span></a>
+                <a class="nav-link pl-3" href="./chart-apexcharts.html"><span class="ml-1 item-text">ApexCharts</span></a>
               </li>
               <li class="nav-item">
                 <a class="nav-link pl-3" href="./datamaps.html"><span class="ml-1 item-text">Datamaps</span></a>
@@ -376,8 +365,7 @@
           </li>
         </ul>
         <div class="btn-box w-100 mt-4 mb-1">
-          <a href="https://themeforest.net/item/tinydash-bootstrap-html-admin-dashboard-template/27511269"
-            target="_blank" class="btn mb-2 btn-primary btn-lg btn-block">
+          <a href="https://themeforest.net/item/tinydash-bootstrap-html-admin-dashboard-template/27511269" target="_blank" class="btn mb-2 btn-primary btn-lg btn-block">
             <i class="fe fe-shopping-cart fe-12 mx-2"></i><span class="small">Buy now</span>
           </a>
         </div>
@@ -400,10 +388,8 @@
                     </div>
                   </div>
                   <div class="form-group">
-                    <button type="button" class="btn btn-sm"><span
-                        class="fe fe-refresh-ccw fe-16 text-muted"></span></button>
-                    <button type="button" class="btn btn-sm mr-2"><span
-                        class="fe fe-filter fe-16 text-muted"></span></button>
+                    <button type="button" class="btn btn-sm"><span class="fe fe-refresh-ccw fe-16 text-muted"></span></button>
+                    <button type="button" class="btn btn-sm mr-2"><span class="fe fe-filter fe-16 text-muted"></span></button>
                   </div>
                 </form>
               </div>
@@ -411,53 +397,52 @@
           </div>
         </div>
       </div>
-       <!-- / ACA EMPIEZA GOOK -->
-  <article>
-	<header style="background-image: url('https://images.unsplash.com/photo-1436891620584-47fd0e565afb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80');">
-		<div class="upper-header">
-			<div class="mini-title">NUEVOS AÑADIDOS</div>
-			<div class="date-since">
-				<p><span class="date-value" id="sinceData"></span></p>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-					<defs>
-						<style>
-							.d {
-								width: 20px;
-								fill: #fff;
-								opacity: .5;
-							}
-						</style>
-					</defs>
-					<path class="d" d="M15,0C6.75,0,0,6.75,0,15s6.75,15,15,15,15-6.75,15-15S23.25,0,15,0Zm7.35,16.65h-7.35c-.83,0-1.5-.67-1.5-1.5V7.8c0-.9,.6-1.5,1.5-1.5s1.5,.6,1.5,1.5v5.85h5.85c.9,0,1.5,.6,1.5,1.5s-.6,1.5-1.5,1.5Z" />
-				</svg>
-			</div>
-		</div>
-		<div class="lower-header">
-			<div class="tags-container">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-					<defs>
-						<style>
-							.d {
-								width: 20px;
-								fill: #fff;
-								opacity: .75;
-							}
-						</style>
-					</defs>
-					<path class="d" d="M19.22,9.66L10.77,1.21c-.74-.74-1.86-1.21-2.97-1.21H1.67C.75,0,0,.75,0,1.67V7.8c0,1.11,.46,2.23,1.3,2.97l8.45,8.46c1,1,2.62,1,3.62,0l5.94-5.95c.93-.93,.93-2.6-.09-3.62ZM6.96,6.35c-.59,.59-1.56,.59-2.15,0-.59-.59-.59-1.56,0-2.15,.59-.59,1.56-.59,2.15,0,.59,.59,.59,1.56,0,2.15Z" />
-				</svg>
-				<span>Literatura</span><span>Juvenil</span>
-			</div>
-			<h1 class="title">Libros,Libros Los mas nuevos</h1>
-			<p class="subtitle"></p>
-		</div>
-	</header>
-</article>
+      <!-- / ACA EMPIEZA GOOK -->
+      <article>
+        <header style="background-image: url('https://images.unsplash.com/photo-1436891620584-47fd0e565afb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80');">
+          <div class="upper-header">
+            <div class="mini-title">NUEVOS AÑADIDOS</div>
+            <div class="date-since">
+              <p><span class="date-value" id="sinceData"></span></p>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <defs>
+                  <style>
+                    .d {
+                      width: 20px;
+                      fill: #fff;
+                      opacity: .5;
+                    }
+                  </style>
+                </defs>
+                <path class="d" d="M15,0C6.75,0,0,6.75,0,15s6.75,15,15,15,15-6.75,15-15S23.25,0,15,0Zm7.35,16.65h-7.35c-.83,0-1.5-.67-1.5-1.5V7.8c0-.9,.6-1.5,1.5-1.5s1.5,.6,1.5,1.5v5.85h5.85c.9,0,1.5,.6,1.5,1.5s-.6,1.5-1.5,1.5Z" />
+              </svg>
+            </div>
+          </div>
+          <div class="lower-header">
+            <div class="tags-container">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <defs>
+                  <style>
+                    .d {
+                      width: 20px;
+                      fill: #fff;
+                      opacity: .75;
+                    }
+                  </style>
+                </defs>
+                <path class="d" d="M19.22,9.66L10.77,1.21c-.74-.74-1.86-1.21-2.97-1.21H1.67C.75,0,0,.75,0,1.67V7.8c0,1.11,.46,2.23,1.3,2.97l8.45,8.46c1,1,2.62,1,3.62,0l5.94-5.95c.93-.93,.93-2.6-.09-3.62ZM6.96,6.35c-.59,.59-1.56,.59-2.15,0-.59-.59-.59-1.56,0-2.15,.59-.59,1.56-.59,2.15,0,.59,.59,.59,1.56,0,2.15Z" />
+              </svg>
+              <span>Literatura</span><span>Juvenil</span>
+            </div>
+            <h1 class="title">Libros,Libros Los mas nuevos</h1>
+            <p class="subtitle"></p>
+          </div>
+        </header>
+      </article>
 
       <div class="card-body"></div>
       <!-- / .ACA SE MUESTRA LOS ACCE DIRECTOS -->
-      <div class="modal fade modal-shortcut modal-slide" tabindex="-1" role="dialog" aria-labelledby="defaultModalLabel"
-        aria-hidden="true">
+      <div class="modal fade modal-shortcut modal-slide" tabindex="-1" role="dialog" aria-labelledby="defaultModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
@@ -548,42 +533,35 @@
   <script src='js/uppy.min.js'></script>
   <script src='js/quill.min.js'></script>
   <script>
-    $('.select2').select2(
-      {
-        theme: 'bootstrap4',
-      });
-    $('.select2-multi').select2(
-      {
-        multiple: true,
-        theme: 'bootstrap4',
-      });
-    $('.drgpicker').daterangepicker(
-      {
-        singleDatePicker: true,
-        timePicker: false,
-        showDropdowns: true,
-        locale:
-        {
-          format: 'MM/DD/YYYY'
-        }
-      });
-    $('.time-input').timepicker(
-      {
-        'scrollDefault': 'now',
-        'zindex': '9999' /* fix modal open */
-      });
+    $('.select2').select2({
+      theme: 'bootstrap4',
+    });
+    $('.select2-multi').select2({
+      multiple: true,
+      theme: 'bootstrap4',
+    });
+    $('.drgpicker').daterangepicker({
+      singleDatePicker: true,
+      timePicker: false,
+      showDropdowns: true,
+      locale: {
+        format: 'MM/DD/YYYY'
+      }
+    });
+    $('.time-input').timepicker({
+      'scrollDefault': 'now',
+      'zindex': '9999' /* fix modal open */
+    });
     /** date range picker */
     if ($('.datetimes').length) {
-      $('.datetimes').daterangepicker(
-        {
-          timePicker: true,
-          startDate: moment().startOf('hour'),
-          endDate: moment().startOf('hour').add(32, 'hour'),
-          locale:
-          {
-            format: 'M/DD hh:mm A'
-          }
-        });
+      $('.datetimes').daterangepicker({
+        timePicker: true,
+        startDate: moment().startOf('hour'),
+        endDate: moment().startOf('hour').add(32, 'hour'),
+        locale: {
+          format: 'M/DD hh:mm A'
+        }
+      });
     }
     var start = moment().subtract(29, 'days');
     var end = moment();
@@ -591,124 +569,110 @@
     function cb(start, end) {
       $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
     }
-    $('#reportrange').daterangepicker(
-      {
-        startDate: start,
-        endDate: end,
-        ranges:
-        {
-          'Today': [moment(), moment()],
-          'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month': [moment().startOf('month'), moment().endOf('month')],
-          'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        }
-      }, cb);
+    $('#reportrange').daterangepicker({
+      startDate: start,
+      endDate: end,
+      ranges: {
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      }
+    }, cb);
     cb(start, end);
-    $('.input-placeholder').mask("00/00/0000",
-      {
-        placeholder: "__/__/____"
-      });
-    $('.input-zip').mask('00000-000',
-      {
-        placeholder: "____-___"
-      });
-    $('.input-money').mask("#.##0,00",
-      {
-        reverse: true
-      });
+    $('.input-placeholder').mask("00/00/0000", {
+      placeholder: "__/__/____"
+    });
+    $('.input-zip').mask('00000-000', {
+      placeholder: "____-___"
+    });
+    $('.input-money').mask("#.##0,00", {
+      reverse: true
+    });
     $('.input-phoneus').mask('(000) 000-0000');
     $('.input-mixed').mask('AAA 000-S0S');
-    $('.input-ip').mask('0ZZ.0ZZ.0ZZ.0ZZ',
-      {
-        translation:
-        {
-          'Z':
-          {
-            pattern: /[0-9]/,
-            optional: true
-          }
-        },
-        placeholder: "___.___.___.___"
-      });
+    $('.input-ip').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
+      translation: {
+        'Z': {
+          pattern: /[0-9]/,
+          optional: true
+        }
+      },
+      placeholder: "___.___.___.___"
+    });
     // editor
     var editor = document.getElementById('editor');
     if (editor) {
       var toolbarOptions = [
-        [
-          {
-            'font': []
-          }],
-        [
-          {
-            'header': [1, 2, 3, 4, 5, 6, false]
-          }],
+        [{
+          'font': []
+        }],
+        [{
+          'header': [1, 2, 3, 4, 5, 6, false]
+        }],
         ['bold', 'italic', 'underline', 'strike'],
         ['blockquote', 'code-block'],
-        [
-          {
+        [{
             'header': 1
           },
           {
             'header': 2
-          }],
-        [
-          {
+          }
+        ],
+        [{
             'list': 'ordered'
           },
           {
             'list': 'bullet'
-          }],
-        [
-          {
+          }
+        ],
+        [{
             'script': 'sub'
           },
           {
             'script': 'super'
-          }],
-        [
-          {
+          }
+        ],
+        [{
             'indent': '-1'
           },
           {
             'indent': '+1'
-          }], // outdent/indent
-        [
-          {
-            'direction': 'rtl'
-          }], // text direction
-        [
-          {
+          }
+        ], // outdent/indent
+        [{
+          'direction': 'rtl'
+        }], // text direction
+        [{
             'color': []
           },
           {
             'background': []
-          }], // dropdown with defaults from theme
-        [
-          {
-            'align': []
-          }],
+          }
+        ], // dropdown with defaults from theme
+        [{
+          'align': []
+        }],
         ['clean'] // remove formatting button
       ];
-      var quill = new Quill(editor,
-        {
-          modules:
-          {
-            toolbar: toolbarOptions
-          },
-          theme: 'snow'
-        });
+      var quill = new Quill(editor, {
+        modules: {
+          toolbar: toolbarOptions
+        },
+        theme: 'snow'
+      });
     }
     // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function () {
+    (function() {
       'use strict';
-      window.addEventListener('load', function () {
+      window.addEventListener('load', function() {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-          form.addEventListener('submit', function (event) {
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
             if (form.checkValidity() === false) {
               event.preventDefault();
               event.stopPropagation();
@@ -722,19 +686,17 @@
   <script>
     var uptarg = document.getElementById('drag-drop-area');
     if (uptarg) {
-      var uppy = Uppy.Core().use(Uppy.Dashboard,
-        {
-          inline: true,
-          target: uptarg,
-          proudlyDisplayPoweredByUppy: false,
-          theme: 'dark',
-          width: 770,
-          height: 210,
-          plugins: ['Webcam']
-        }).use(Uppy.Tus,
-          {
-            endpoint: 'https://master.tus.io/files/'
-          });
+      var uppy = Uppy.Core().use(Uppy.Dashboard, {
+        inline: true,
+        target: uptarg,
+        proudlyDisplayPoweredByUppy: false,
+        theme: 'dark',
+        width: 770,
+        height: 210,
+        plugins: ['Webcam']
+      }).use(Uppy.Tus, {
+        endpoint: 'https://master.tus.io/files/'
+      });
       uppy.on('complete', (result) => {
         console.log('Upload complete! We’ve uploaded these files:', result.successful)
       });
@@ -752,7 +714,8 @@
     gtag('js', new Date());
     gtag('config', 'UA-56159088-1');
   </script>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js'></script><script  src="js/scriptlaunc.js"></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js'></script>
+  <script src="js/scriptlaunc.js"></script>
 </body>
 
 </html>
