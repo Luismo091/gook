b68f8b286b89eab147d32afc9b53abb8c05c7349
AndresD4rk <abarrera2@udi.edu.co>
2022-11-05 23:45:34 -0500

    123

--- proyectorden/GOOK/main.php
+++ proyectorden/GOOK/main.php
@@ -35,7 +35,7 @@ session_start();
   <div class="wrapper">
     <nav class="topnav navbar navbar-light">
       <button type="button" class="navbar-toggler text-muted mt-2 p-0 mr-3 collapseSidebar">
-        <i class="fe fe-menu navbar-toggler-icon"></i>
+          <i class="fe fe-menu navbar-toggler-icon"></i>
       </button>
       <form class="form-inline mr-auto searchform text-muted">
         <input class="form-control mr-sm-2 bg-transparent border-0 pl-4 text-muted" type="search" placeholder="Busca algo..." aria-label="Search">
@@ -61,7 +61,6 @@ session_start();
           <a class="nav-link dropdown-toggle text-muted pr-0" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="avatar avatar-sm mt-2">
             <img src="data:image/png;base64,<?=base64_encode($_SESSION["foto"]) ?>">
-            <a><?=$_SESSION["id"] ?></a>
             </span>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
