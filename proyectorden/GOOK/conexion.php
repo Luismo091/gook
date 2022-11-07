074b36b3168cd05eedd9d9c110e4f67f6e3bdc35
Luismo091 <101841546+Luismo091@users.noreply.github.com>
2022-11-06 22:39:48 -0500

    BD

--- proyectorden/GOOK/conexion.php
+++ proyectorden/GOOK/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $host = '34.173.247.77';
 $user = "root";
-$pass = 'v6"2VROLrR9cenO@';
+$pass = 'jZG|E"9iQZc-TUlv';
 $db = "GookPochita";
 $conexion = mysqli_connect($host, $user, $pass, $db);
 mysqli_set_charset($conexion, "utf8mb4");
