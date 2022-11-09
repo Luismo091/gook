a41986d53c007c3f73073fe5b509cdcba58ef433
AndresD4rk <abarrera2@udi.edu.co>
2022-11-09 12:20:31 -0500

    123123

--- proyectorden/GOOK/conexion.php
+++ proyectorden/GOOK/conexion.php
@@ -1,7 +1,7 @@
 <?php
-$host = '34.173.247.77';
-$user = "root";
-$pass = 'jZG|E"9iQZc-TUlv';
+$host = 'db-pochitagook-do-user-12851402-0.b.db.ondigitalocean.com';
+$user = "doadmin";
+$pass = 'AVNS_DDR4II2qCyCqkPg7nPK';
 $db = "GookPochita";
 $conexion = mysqli_connect($host, $user, $pass, $db);
 mysqli_set_charset($conexion, "utf8mb4");
