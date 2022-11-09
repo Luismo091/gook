2d587faa16500b0eb2b43f1c1d75be19235a21b2
AndresD4rk <abarrera2@udi.edu.co>
2022-11-09 12:38:03 -0500

    31231

--- proyectorden/GOOK/conexion.php
+++ proyectorden/GOOK/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'db-pochitagook-do-user-12851402-0.b.db.ondigitalocean.com';
+$host = 'db-pochitagook-do-user-12851402-0.b.db.ondigitalocean.com:25060';
 $user = "doadmin";
 $pass = 'AVNS_DDR4II2qCyCqkPg7nPK';
 $db = "GookPochita";
