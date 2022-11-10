59305d14f9f0185532d3bf2bfb26324abe0824aa
AndresD4rk <abarrera2@udi.edu.co>
2022-11-10 00:39:47 -0500

    31233

--- proyectorden/GOOK/fav.php
+++ proyectorden/GOOK/fav.php
@@ -1,20 +1,30 @@
 <?php
 session_start();
+
 include "conexion.php";
 if (empty($_SESSION['id'])) {
   header("location:page-404.html");
 }
 
-if (isset($_GET["variablel"])) {
-  $consulta = ($_GET['variablel']);
-  $variable = $consulta;
+if (isset($_GET["variable1"])) {
+  $variable = ($_GET['variable1']);
 }
 $idusu = $_SESSION['id'];
-$d=rand(1,3000);
-$sql = $conexion->query("INSERT INTO Favorito (idFav, Libro_idLib, Usu_idusu) VALUES ('$d','$variable','$idusu')");
-if ($sql) {
-    header("location:fichabook.php?variable=$variable");
+$sql1 = $conexion->query("SELECT * FROM Favorito WHERE Libro_idLib='$variable' AND Usu_idusu='$idusu'");
+$datos=$sql1->fetch_object();
+if (!empty($datos->idFav)) {
+  $sql2 = $conexion->query("DELETE FROM Favorito WHERE (`idFav` = '$datos->idFav'");
+  echo "<script>alert('Holis')</script>";
+  //header("location:fichabook.php?variable=$variable");
 } else {
-
+  $sql = $conexion->query("INSERT INTO Favorito (Libro_idLib, Usu_idusu) VALUES ('$variable','$idusu')");
+  if ($sql) {
+      //header("location:fichabook.php?variable=$variable");
+      echo "<script>alert('Holis2')</script>";
+  } else {
+  
+  }
 }
+
+
 ?>
