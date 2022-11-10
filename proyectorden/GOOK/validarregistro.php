86100962524ff889ec87a5e53491984fd3492a3e
AndresD4rk <abarrera2@udi.edu.co>
2022-11-10 12:46:49 -0500

    1231

--- proyectorden/GOOK/validarregistro.php
+++ proyectorden/GOOK/validarregistro.php
@@ -1,6 +1,6 @@
 <?php
  session_start();
- 
+ include "conexion.php";
  if (empty($_GET['inputescritor'])) {    
     $chec = "0";
 }else{
@@ -18,5 +18,38 @@ $clac = $_GET["inputpasswordconfi"];
 
 
 echo "INSERT INTO `GookPochita`.`Usuario` (`idUsu`, `nom1`, `nom2`, `ape1`, `ape2`, `eda`, `Seguridad_idSeg`, `Suscripcion_idSus`) VALUES ('5', '1', '1', '1', '1', '1', '5', '1')";
+echo "INSERT INTO `GookPochita`.`Seguridad` (`idSeg`, `email`, `clave`, `Rol_idRol`) VALUES ('5', '5', '5', '5'); ";
+
+$idSEG;
+$sql1 = $conexion->query("SELECT max(idSeg) from Seguridad;");
+$datos=$sql1->fetch_array();
+$vadatos=$datos["max(idSeg)"];
+if (empty($datos["max(idSeg)"])) {
+$idSEG=1;
+}else{
+    $idSEG=$vadatos;
+    $idSEG=$idSEG+1;
+}
+
+$sql = $conexion->query("INSERT INTO
+ Seguridad (idSeg, email, clave, Rol_idRol)
+ VALUES ('$idSEG','$email','$clav','$chec')");
+  if ($sql) {
+    $sql = $conexion->query("INSERT INTO
+ Usuario (nom1, nom2, ape1, ape2, eda, Seguridad_idSeg)
+ VALUES ('$nom1','$nom2','$ape1','$ape2','$edad','$idSEG')");
+  if ($sql) {
+      header("");
+  } else {
+  
+  }
+      header("");
+  } else {
+  
+  }
+
+
+
+?>
 
 ?>
