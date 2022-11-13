ca35d13aec1a716c2b82e27041b48b6e5427dab1
AndresD4rk <abarrera2@udi.edu.co>
2022-11-12 20:51:01 -0500

    123

--- proyectorden/GOOK/main.php
+++ proyectorden/GOOK/main.php
@@ -369,43 +369,10 @@ function enviar_formulario(){
 
             </div>
 
-            <!-- / ACA EMPIEZA GOOK -->
-            <div class="col-12 col-lg-9">
-              <div class="card-header">
-                <strong class="card-title">Aclamados por la critica</strong>
-              </div>
-              <div class="row">
-                <div class="container swiper">
-                  <div class="slide-container">
-                    <div class="card-wrapper swiper-wrapper">
-                      <?php
+          
+            </div> -->
 
-                      $sql = $conexion->query("SELECT * from Libro order by lecturas DESC");
-                      while ($datos = $sql->fetch_object()) {
-                      ?>
-                        <div class="card swiper-slide">
-                          <div class="image-box">
-                            <img src="data:image/jpg;base64,<?php echo base64_encode($datos->imagen) ?>" alt="" />
-                          </div>
-                          <div class="profile-details">
-                            <img src="data:image/jpg;base64,<?php echo base64_encode($datos->imagen) ?>" alt="" />
-                            <div class="name-job">
-                              <h3 class="name"><?= $datos->titLib ?></h3>
-                              <h4 class="name"><?= $datos->fecPub ?></h4>
-                              <a class="nav-link text-muted my-2" href="fichabook.php?variable=<?= $datos->idLib ?>" class="subtitle"><span class="fe fe-arrow-right fe-16"></span></a>
-                            </div>
-                          </div>
-                        </div>
-                      <?php }
-                      ?>
-                    </div>
-                  </div>
-                  <div class="swiper-button-next swiper-navBtn"></div>
-                  <div class="swiper-button-prev swiper-navBtn"></div>
-                  <div class="swiper-pagination"></div>
-                </div>
-              </div>
-            </div>
+
             <div class="row">
               <div class="col-md-6 mb-4">
                 <div class="card-body">
