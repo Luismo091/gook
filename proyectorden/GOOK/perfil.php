a1586644feb5362a4147386a6879be4904742bdb
AndresD4rk <abarrera2@udi.edu.co>
2022-11-13 19:53:45 -0500

    asd

--- proyectorden/GOOK/perfil.php
+++ proyectorden/GOOK/perfil.php
@@ -32,58 +32,68 @@ include "conexion.php";
   <link rel="stylesheet" href="css/daterangepicker.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/stylef.css">
+  <link rel="stylesheet" href="css/stylelaunch.css">
 
 </head>
 
 <body class="vertical  dark  ">
   <div class="wrapper">
-    <nav class="topnav navbar navbar-light">
+  <nav class="topnav navbar navbar-light">
       <button type="button" class="navbar-toggler text-muted mt-2 p-0 mr-3 collapseSidebar">
         <i class="fe fe-menu navbar-toggler-icon"></i>
       </button>
-      <form class="form-inline mr-auto searchform text-muted">
-        <input class="form-control mr-sm-2 bg-transparent border-0 pl-4 text-muted" type="search" placeholder="Type something..." aria-label="Search">
+
+      <form name="filform"class="form-inline mr-auto searchform text-muted" action="filtropage.php" method="GET">
+
+        <input name="filtxt" class="form-control mr-sm-2 bg-transparent border-0 pl-4 text-muted" type="search" placeholder="Busca algo..." aria-label="Search">
+        <select name="filcat"class="form-control mr-sm-2 bg-transparent border-0 pl-4 text-muted" style="width: 200px;">
+          <option style="background-color:#212529; color:#6c757d">Seleccione una...</option>
+          <?php
+          $result = mysqli_query($conexion, 'SELECT * FROM Categoria');
+          while ($row = mysqli_fetch_assoc($result)) {
+            echo "<option style='background-color:#212529; color:#6c757d' value='$row[idCat]'>$row[nomCat]</option>";
+          }
+          ?>
+        </select>
+        <a  href="javascript:enviar_formulario()"> 
+          <span style="position: absolute; " class="fe fe-arrow-right fe-16"></span>
+        </a>
+        
+      
       </form>
+
+      <script>
+function enviar_formulario(){
+   document.filform.submit()
+}
+</script>
+
+
+
+
       <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link text-muted my-2" href="#" id="modeSwitcher" data-mode="dark">
-            <i class="fe fe-sun fe-16"></i>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-muted my-2" href="./#" data-toggle="modal" data-target=".modal-shortcut">
-            <span class="fe fe-grid fe-16"></span>
-          </a>
-        </li>
-        <li class="nav-item nav-notif">
-          <a class="nav-link text-muted my-2" href="./#" data-toggle="modal" data-target=".modal-notif">
-            <span class="fe fe-bell fe-16"></span>
-            <span class="dot dot-md bg-success"></span>
-          </a>
-        </li>
+
+        
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle text-muted pr-0" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a href="perfil.php">
             <span class="avatar avatar-sm mt-2">
-              <img src="data:image/png;base64,<?= base64_encode($_SESSION["foto"]) ?>" alt="..." class="avatar-img rounded-circle">
+              <img src="data:image/png;base64,<?= base64_encode($_SESSION["foto"]) ?>">
             </span>
           </a>
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Profile</a>
-            <a class="dropdown-item" href="#">Settings</a>
-            <a class="dropdown-item" href="#">Activities</a>
-          </div>
         </li>
       </ul>
     </nav>
+    
+  <!--Menu Lateral-->
     <aside class="sidebar-left border-right bg-white shadow" id="leftSidebar" data-simplebar>
       <a href="#" class="btn collapseSidebar toggle-btn d-lg-none text-muted ml-2 mt-3" data-toggle="toggle">
         <i class="fe fe-x"><span class="sr-only"></span></i>
       </a>
+
       <nav class="vertnav navbar navbar-light">
         <!-- nav bar -->
         <div class="w-100 mb-4 d-flex">
           <a class="navbar-brand mx-auto mt-2 flex-fill text-center">
-
             <svg width="auto" height="100px" viewBox="0 0 1920 645.52203" version="1.1" id="svg5" xml:space="preserve" inkscape:export-filename="..\..\..\..\..\..\..\..\Good Book\IconoGookFullAzul.svg" inkscape:export-xdpi="96" inkscape:export-ydpi="96" inkscape:version="1.2.1 (9c6d41e410, 2022-07-14)" sodipodi:docname="LogoPoquitaChiquito.svg" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
               <sodipodi:namedview id="namedview7" pagecolor="#ffffff" bordercolor="#000000" borderopacity="0.25" inkscape:showpageshadow="2" inkscape:pageopacity="0.0" inkscape:pagecheckerboard="0" inkscape:deskcolor="#d1d1d1" inkscape:document-units="px" showgrid="true" inkscape:zoom="0.5" inkscape:cx="771.00001" inkscape:cy="520" inkscape:window-width="1920" inkscape:window-height="1017" inkscape:window-x="-8" inkscape:window-y="-8" inkscape:window-maximized="1" inkscape:current-layer="g509" />
               <defs id="defs2" />
@@ -100,7 +110,6 @@ include "conexion.php";
             </svg>
           </a>
         </div>
-
         <p class="text-muted nav-heading mt-4 mb-1">
           <span>GOOK</span>
         </p>
@@ -112,27 +121,32 @@ include "conexion.php";
             </a>
           </li>
           <li class="nav-item w-100">
-            <a class="nav-link" href="main.php">
+            <a class="nav-link" href="favoritos.php">
               <i class="fe fe-heart fe-16"></i>
               <span class="ml-3 item-text">Favoritos</span>
             </a>
           </li>
           <li class="nav-item w-100">
-            <a class="nav-link" href="main.php">
+            <a class="nav-link" href="perfil.php">
               <i class="fe fe-user fe-16"></i>
               <span class="ml-3 item-text">Perfil</span>
             </a>
           </li>
           <li class="nav-item w-100">
-            <a class="nav-link" href="main.php">
-              <i class="fe fe-profile fe-16"></i>
-              <span class="ml-3 item-text">Home</span>
+            <a class="nav-link" href="sesion.php">
+              <i class="fe fe-log-out fe-16"></i>
+              <span class="ml-3 item-text">Salir</span>
             </a>
           </li>
+
         </ul>
       </nav>
-
     </aside>
+
+
+
+
+
     <main role="main" class="main-content">
       <div class="container-fluid">
         <div class="row justify-content-center">
