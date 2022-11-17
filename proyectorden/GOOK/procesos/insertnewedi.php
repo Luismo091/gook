e1ba7f79a78a9d33fe3463c582fe8ccbeea4c380
AndresD4rk <abarrera2@udi.edu.co>
2022-11-17 11:37:11 -0500

    123

---
+++ proyectorden/GOOK/procesos/insertnewedi.php
@@ -0,0 +1,42 @@
+<?php
+
+ include "conexion.php";
+ if (empty($_POST['idEdi'])) {    
+    echo'<script type="text/javascript">
+    alert("Error Informe a Soporte");
+    history.back();
+    </script>';
+}else{
+    $idEdi = $_POST["idEdi"];
+    $nomedi = $_POST["nomedi"];
+   
+    
+   
+
+    if(isset($_FILES['imagenedi']['name'])){
+        $foto= addslashes(file_get_contents($_FILES['imagenedi']['tmp_name']));
+
+    $sql = $conexion->query("INSERT INTO
+         Editorial (idEdi, nomEdi, foto_edi)
+         VALUES ('$idEdi','$nomedi','$foto')");
+          if ($sql) {
+            echo'<script type="text/javascript">
+            alert("Autor Registrada");
+            </script>';
+            header("location:../administrador/listeditoriales.php");
+           
+          } else {
+            echo'<script type="text/javascript">
+            alert("Error");
+            history.back();
+            </script>';
+          }
+}
+echo'<script type="text/javascript">
+            alert("Error");
+            history.back();
+            </script>';
+}
+?>
+
+
