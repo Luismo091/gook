eef65c7de126e8a1402702808b03aa93f220e3e0
AndresD4rk <abarrera2@udi.edu.co>
2022-11-19 21:08:40 -0500

    123

--- proyectorden/GOOK/filtropage.php
+++ proyectorden/GOOK/filtropage.php
@@ -214,11 +214,15 @@ include "procesos/conexion.php";
                                     <div class="col-md-2"">
                                 <div class="card shadow" >
                                     <div class="card-body">
-                                        <p>' . $titulo . '</p>'; ?>
+                                       '; ?>
                 <div class="image-box" style="height: 380px;">
                   <img src="data:image/png;base64,<?= $imli ?>">
                 </div>
-              <?php echo '<p style="margin-top: 2px;"></p><p>' . $autor . '</p>
+              <?php echo '<div style="height:21px;">
+                                          <p>' . $titulo . '</p>
+                                          </div>
+              
+              <p style="margin-top: 2px;"></p><p>' . $autor . '</p>
                                         <p>' . $editorial . ' <a class="nav-link" href="fichabook.php?variable=' . $idLib . '?>">                            
                                         <span class="ml-3 item-text">Ver</span>
                                         <i class="fe fe-arrow-right"></i>
