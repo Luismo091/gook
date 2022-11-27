f460a59f7d556118bbf0346b2ba96216ab585764
AndresD4rk <abarrera2@udi.edu.co>
2022-11-27 00:20:46 -0500

    123

--- proyectorden/GOOK/editor/listbooked.php
+++ proyectorden/GOOK/editor/listbooked.php
@@ -121,57 +121,244 @@ include "../procesos/conexion.php";
       </nav>
     </aside>
     <main role="main" class="main-content">
-    
+      <div class="container-fluid">
+          <div class="row justify-content-center">
+            
+              
+             
+              <div class="col-10">
+                <h2 class="mb-2 page-title">Libros</h2>
+                </div>
+                
+                <div  class="col-2">
+                <a href="insertbook.php">
+              <span class="fe fe-plus fe-16 mr-3"></span>Nuevo Libro
+            
+              </a>
+            </div>
+          
 
 
+              <div class="col-12">
+              
+              <div class="row my-4">
+                <!-- Small table -->
+                <div class="col-md-12">
+                  <div class="card shadow">
+                    <div class="card-body">
+                      <!-- table -->
+                      <table class="table datatables" id="dataTable-1">
+                        <thead>
+                          <tr>
+                          
+                            <th>#</th>
+                            <th>Titulo</th>
+                            <th>Publicación</th>
+                            <th>Fecha Libro</th>
+                            <th>Sinopsis</th>
+                            <th>Imagen</th>
+                            <th>Categoria</th>
+                            <th>Autor</th>
+                            <th>Editorial</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+
+<?php
+                  
+              
+                      $sql = $conexion->query("SELECT idLib, titLib, fecPub,fecLib, sinopsis, imagen,nomAut1,nomAut2, apeAut1,apeAut2, nomCat, nomEdi
+                      FROM Libro
+                            INNER JOIN LibAut
+                            ON Libro.idLib=LibAut.Libro_idLib
+                            INNER JOIN Autor
+                            ON Autor.idAut=LibAut.Autor_idAut
+                            INNER JOIN LibEdi
+                            ON Libro.idLib=LibEdi.Libro_idLib
+                            INNER JOIN Editorial
+                            ON Editorial.idEdi=LibEdi.Editorial_idEdi
+                            INNER JOIN Categoria
+                            ON Libro.Categoria_idCat=Categoria.idCat
+                            WHERE estado!=1;
+                            
+                            ");
+  
+                       while ($datos = $sql->fetch_array()) {
+                           $idLib = $datos['idLib'];
+                           $titLib = $datos['titLib'];
+                           $fecPub = $datos['fecPub'];
+                           $fecLib = $datos['fecLib'];
+                           $sinopsis = $datos['sinopsis'];
+                           $imagen = $datos['imagen'];
+                           $nomAut = $datos['nomAut1']." ".$datos['nomAut2']." ".$datos['apeAut1']." ".$datos['apeAut2'];
+                           $nomCat = $datos['nomCat'];
+                           $nomEdi = $datos['nomEdi'];
+                       echo "<tr> 
+                          <td>$idLib</td>
+                       <td>$titLib</td>  
+                       <td>$fecPub</td> 
+                       <td>$fecLib</td> 
+                       <td>$sinopsis</td>  
+                       <td>";
+                       ?>
+                       <span class="avatar avatar-sm mt-2">
+              <img src="data:image/png;base64,<?= base64_encode($imagen) ?>">
+            </span>
+            <?php
+                       echo "</td>
+                       <td>$nomCat</td> 
+                       <td>$nomAut</td> 
+                       <td>$nomEdi</td>  
+                       ";
+                           
 
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          </main> <!-- main -->
+                       
+                        echo'<td><button class="btn btn-sm dropdown-toggle more-horizontal" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="text-muted sr-only">Action</span>
+                      </button>
+                      <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="editbook.php?variable='.$idLib.'">Editar</a>
+                        <a class="dropdown-item" href="deletebook.php?variable='.$idLib.'">Eliminar</a>
+                      </div>
+                    </td>
+                  </tr>';
+                       }                                       
+?>                                                                                                                                            
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div> <!-- simple table -->
+              </div> <!-- end section -->
+            </div> <!-- .col-12 -->
+          </div> <!-- .row -->
+        </div> <!-- .container-fluid -->
+        <div class="modal fade modal-notif modal-slide" tabindex="-1" role="dialog" aria-labelledby="defaultModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-sm" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="defaultModalLabel">Notifications</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="list-group list-group-flush my-n3">
+                  <div class="list-group-item bg-transparent">
+                    <div class="row align-items-center">
+                      <div class="col-auto">
+                        <span class="fe fe-box fe-24"></span>
+                      </div>
+                      <div class="col">
+                        <small><strong>Package has uploaded successfull</strong></small>
+                        <div class="my-0 text-muted small">Package is zipped and uploaded</div>
+                        <small class="badge badge-pill badge-light text-muted">1m ago</small>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="list-group-item bg-transparent">
+                    <div class="row align-items-center">
+                      <div class="col-auto">
+                        <span class="fe fe-download fe-24"></span>
+                      </div>
+                      <div class="col">
+                        <small><strong>Widgets are updated successfull</strong></small>
+                        <div class="my-0 text-muted small">Just create new layout Index, form, table</div>
+                        <small class="badge badge-pill badge-light text-muted">2m ago</small>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="list-group-item bg-transparent">
+                    <div class="row align-items-center">
+                      <div class="col-auto">
+                        <span class="fe fe-inbox fe-24"></span>
+                      </div>
+                      <div class="col">
+                        <small><strong>Notifications have been sent</strong></small>
+                        <div class="my-0 text-muted small">Fusce dapibus, tellus ac cursus commodo</div>
+                        <small class="badge badge-pill badge-light text-muted">30m ago</small>
+                      </div>
+                    </div> <!-- / .row -->
+                  </div>
+                  <div class="list-group-item bg-transparent">
+                    <div class="row align-items-center">
+                      <div class="col-auto">
+                        <span class="fe fe-link fe-24"></span>
+                      </div>
+                      <div class="col">
+                        <small><strong>Link was attached to menu</strong></small>
+                        <div class="my-0 text-muted small">New layout has been attached to the menu</div>
+                        <small class="badge badge-pill badge-light text-muted">1h ago</small>
+                      </div>
+                    </div>
+                  </div> <!-- / .row -->
+                </div> <!-- / .list-group -->
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary btn-block" data-dismiss="modal">Clear All</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal fade modal-shortcut modal-slide" tabindex="-1" role="dialog" aria-labelledby="defaultModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="defaultModalLabel">Shortcuts</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body px-5">
+                <div class="row align-items-center">
+                  <div class="col-6 text-center">
+                    <div class="squircle bg-success justify-content-center">
+                      <i class="fe fe-cpu fe-32 align-self-center text-white"></i>
+                    </div>
+                    <p>Control area</p>
+                  </div>
+                  <div class="col-6 text-center">
+                    <div class="squircle bg-primary justify-content-center">
+                      <i class="fe fe-activity fe-32 align-self-center text-white"></i>
+                    </div>
+                    <p>Activity</p>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col-6 text-center">
+                    <div class="squircle bg-primary justify-content-center">
+                      <i class="fe fe-droplet fe-32 align-self-center text-white"></i>
+                    </div>
+                    <p>Droplet</p>
+                  </div>
+                  <div class="col-6 text-center">
+                    <div class="squircle bg-primary justify-content-center">
+                      <i class="fe fe-upload-cloud fe-32 align-self-center text-white"></i>
+                    </div>
+                    <p>Upload</p>
+                  </div>
+                </div>
+                <div class="row align-items-center">
+                  <div class="col-6 text-center">
+                    <div class="squircle bg-primary justify-content-center">
+                      <i class="fe fe-users fe-32 align-self-center text-white"></i>
+                    </div>
+                    <p>Users</p>
+                  </div>
+                  <div class="col-6 text-center">
+                    <div class="squircle bg-primary justify-content-center">
+                      <i class="fe fe-settings fe-32 align-self-center text-white"></i>
+                    </div>
+                    <p>Settings</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main> <!-- main -->
         </div> <!-- .wrapper -->
         <script src="../js/jquery.min.js"></script>
         <script src="../js/popper.min.js"></script>
--- proyectorden/GOOK/procesos/insertnewbookescritor.php
+++ proyectorden/GOOK/procesos/insertnewbookescritor.php
@@ -11,10 +11,115 @@ if (empty($_POST['idLib'])) {
     $idLib = $_POST["idLib"];
     $sql2 = $conexion->query("SELECT * FROM escrilib  WHERE usuid=$idusu");
     if ($datos2 = $sql2->fetch_array()) {
-        echo '<script type="text/javascript">
-        alert("XD");
-        history.back();
-        </script>';
+        $idAut = $datos2['autid'];
+        $file = $_FILES["fileTest"]["name"]; //Nombre de nuestro archivo
+            $validator = 1; //Variable validadora
+            $file_type = strtolower(pathinfo($file, PATHINFO_EXTENSION)); //Extensión de nuestro archivo
+            $url_temp = $_FILES["fileTest"]["tmp_name"]; //Ruta temporal a donde se carga el archivo 
+        
+            //dirname(__FILE__) nos otorga la ruta absoluta hasta el archivo en ejecución
+            $url_insert = dirname(__FILE__) . ""; //Carpeta donde subiremos nuestros archivos
+            $prueba = $url_insert;
+            $prueba = str_replace(array("procesos"), "assets\libros/", $prueba);
+            //Ruta donde se guardara el archivo, usamos str_replace para reemplazar los "\" por "/"
+            $url_target = str_replace('\\', '/', $prueba) . '' . $file;
+        
+            //Si la carpeta no existe, la creamos
+            if (!file_exists($prueba)) {
+                mkdir($prueba, 0777, true);
+            };
+        
+            //Validamos el tamaño del archivo
+        
+        
+            //Validamos la extensión del archivo
+            if ($file_type != "pdf") {
+                echo "Solo se permiten archivos PDF";
+                $validator = 0;
+            }
+        
+            //movemos el archivo de la carpeta temporal a la carpeta objetivo y verificamos si fue exitoso
+            if ($validator == 1) {
+                if (move_uploaded_file($url_temp, $url_target)) {
+                    //echo "El archivo " . htmlspecialchars(basename($file)) . " ha sido cargado con éxito.";
+                    $idLib = $_POST["idLib"];
+                    $titLib = $_POST["titLib"];
+                    $fecLib = $_POST["fecLib"];
+                    $fecPub = $_POST["fecPub"];
+                    $filcat = $_POST["filcat"];
+                   
+                    $langua = $_POST["lang"];
+                    $sinopsis = $_POST["sinopsis"];
+                    $newnamebook = $prueba . $idLib . ".pdf";
+                    rename($url_target, $newnamebook);
+            if (isset($_FILES['imagen']['name'])) {
+                $foto = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
+                $sql = $conexion->query("INSERT INTO
+                Libro (idLib, titLib, fecPub, fecLib, sinopsis, imagen, estado, Categoria_idCat,lan)
+                VALUES ('$idLib','$titLib','$fecPub','$fecLib','$sinopsis','$foto','3','$filcat','$langua')");
+                if ($sql) {
+                    $idLA;
+                    $sql = $conexion->query("SELECT MAX(idLA) FROM LibAut");
+                    if ($datos = $sql->fetch_array()) {
+                        $idLA = $datos['MAX(idLA)'];
+                        $idLA++;
+                    } else {
+                        $idLA = 1;
+                    }
+                    $sql = $conexion->query("INSERT INTO
+                 LibAut (idLA, Libro_idLib, Autor_idAut)
+                 VALUES ('$idLA','$idLib','$idAut')");
+        
+        
+                    $idLE;
+                    $sql = $conexion->query("SELECT MAX(idLE) FROM LibEdi");
+                    if ($datos = $sql->fetch_array()) {
+                        $idLE = $datos['MAX(idLE)'];
+                        $idLE++;
+                    } else {
+                        $idLE = 1;
+                    }
+                    $sql = $conexion->query("INSERT INTO
+                 LibEdi (idLE, Libro_idLib, Editorial_idEdi)
+                 VALUES ('$idLE','$idLib',0)");
+                    
+        
+                } else {
+                }
+            }
+        
+        
+                    $sql3 = $conexion->query("SELECT MAX(idesli) FROM escrilib");
+                    if ($datos3 = $sql3->fetch_array()) {
+                        $idesli = $datos3['MAX(idesli)'];
+                        $idesli++;
+                    } else {
+                        $idesli = 1;
+                    }
+                    $sql4 = $conexion->query("INSERT INTO
+                    escrilib (idesli, libid, usuid, autid)
+                    VALUES ('$idesli','$idLib','$idusu','$idAut');");
+                    if ($sql4) {
+                        echo '<script type="text/javascript">
+                        alert("Libro Subido");
+                        window.location= "../escritor/listbookes.php";
+                        </script>';
+                    
+                    } else {
+                    
+                    }
+        
+        
+        
+        
+        
+        
+                } else {
+                    echo "Ha habido un error al cargar tu archivo.";
+                }
+            } else {
+                echo "Error: el archivo no se ha cargado";
+            }
     }else{
         $sql = $conexion->query("SELECT * FROM Usuario WHERE idUsu=$idusu");
         if ($datos = $sql->fetch_array()) {
@@ -128,7 +233,7 @@ if (empty($_POST['idLib'])) {
                     }
                     $sql4 = $conexion->query("INSERT INTO
                     escrilib (idesli, libid, usuid, autid)
-                    VALUES ('$idesli','$idLib','$idusu','$idLA');");
+                    VALUES ('$idesli','$idLib','$idusu','$idAut');");
                     if ($sql4) {
                         echo '<script type="text/javascript">
                         alert("Libro Subido");
